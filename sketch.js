6d5f76af82461162a6304fd52d6d96bb55c68c50
abhayramS <89374590+abhayramS@users.noreply.github.com>
2022-11-15 18:45:08 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -69,18 +69,18 @@ function draw() {
       
     }else if(jwelleryG.isTouching(boy)) {
       jwelleryG.destroyEach();
-      treasureCollection=treasureCollection+150;
+      treasureCollection= treasureCollection + 150;
       
     }else{
       if(swordGroup.isTouching(boy)) {
-       gameState = END;
-
-       boy.addAnimation("SahilRunning",endImg);
-       boy.x=200;
-       boy.y=300;
-       boy.scale=0.6;
-       
-       cashG.destroyEach();
+        gameState=END;
+        
+        boy.addAnimation("SahilRunning",endImg);
+        boy.x=200;
+        boy.y=300;
+        boy.scale=0.6;
+        
+        cashG.destroyEach();
         diamondsG.destroyEach();
         jwelleryG.destroyEach();
         swordGroup.destroyEach();
@@ -89,6 +89,7 @@ function draw() {
         diamondsG.setVelocityYEach(0);
         jwelleryG.setVelocityYEach(0);
         swordGroup.setVelocityYEach(0);
+     
     }
   }
   
